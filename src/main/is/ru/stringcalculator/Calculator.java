b880de20d371c7ff8c9b0a043fc7b568c9637c26
hjalmarh15 <hjelmut@gmail.com>
2016-10-11 21:50:19 +0000

    Refactor, added function for calculating sum

--- src/main/is/ru/stringcalculator/Calculator.java
+++ src/main/is/ru/stringcalculator/Calculator.java
@@ -6,18 +6,22 @@ public class Calculator {
 			return 0;
 		else if(text.contains(",")) {
 			String[] numbers = text.split(",");
-			int result = 0;
-			for(String number : numbers) {
-				result += toInt(number);
-			}
-			return result;
+			return sum(numbers);
 		}
 		else {
-			return 1;
+			return toInt(text);
 		}
 	}
 
 	private static int toInt(String text) {
 		return Integer.parseInt(text);
+	}
+
+	private static int sum(String [] numbers) {
+		int result = 0;
+		for(String number : numbers) {
+			result += toInt(number);
+		}
+		return result;
 	}
 }
