4d663fcb4fb5706ff824374396a719f8a50dfa2d
hjalmarh15 <hjelmut@gmail.com>
2016-10-11 23:38:13 +0000

    Refactoring, created a function to check for negative numbers in input

--- src/main/is/ru/stringcalculator/Calculator.java
+++ src/main/is/ru/stringcalculator/Calculator.java
@@ -4,8 +4,9 @@ public class Calculator {
 	public static int add(String text) {
 		if(text.equals(""))
 			return 0;
-				
+
 		String [] numbers = text.split(",|\n");
+		checkNegatives(numbers);
 		return sum(numbers);
 	}
 
@@ -15,16 +16,20 @@ public class Calculator {
 
 	private static int sum(String [] numbers) {
 		int result = 0;
-		String negatives = "";
 		for(String number : numbers) {
-			if(toInt(number) < 0) 
-				 negatives += number;
 			if(toInt(number) < 1001)
 				result += toInt(number);
 		}
+		return result;
+	}
 
+	private static void checkNegatives(String[] numbers) {
+		String negatives = "";
+		for(String number : numbers) {
+			if(toInt(number) < 0) 
+				 negatives += number + ", ";
+		}
 		if (!negatives.equals(""))
 			throw new IllegalArgumentException("Negatives not allowed: " + negatives);
-		return result;
 	}
 }
