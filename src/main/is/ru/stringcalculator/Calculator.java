73d8cdf8a1c825129d1ffd105decb03f1159bd34
hjalmarh15 <hjelmut@gmail.com>
2016-10-11 23:26:57 +0000

    Realized I could split a string with two delimiters. Changed the code considerably when I found this out

--- src/main/is/ru/stringcalculator/Calculator.java
+++ src/main/is/ru/stringcalculator/Calculator.java
@@ -4,52 +4,27 @@ public class Calculator {
 	public static int add(String text) {
 		if(text.equals(""))
 			return 0;
-		else if(text.contains("\n")) {
-			String [] newLineSplit = text.split("\n");
-			return sum(newLineSplit, "\n");
-		}
-		else if(text.contains(",")) {
-			String[] numbers = text.split(",");
-			return sum(numbers, ",");
-		}
-		else {
-			return toInt(text);
-		}
+				
+		String [] numbers = text.split(",|\n");
+		return sum(numbers);
 	}
 
 	private static int toInt(String text) {
 		return Integer.parseInt(text);
 	}
 
-	private static int sum(String [] numbers, String delimiter) {
+	private static int sum(String [] numbers) {
 		int result = 0;
 		String negatives = "";
-		if(delimiter.equals("\n")) {
-			for(String string : numbers ) {
-				if(string.contains(",")) {
-					String[] moreNumbers = string.split(",");
-					result += sum(moreNumbers, ",");
-				}
-				else {
-					if(toInt(string) < 1001)
-						result += toInt(string);
-				}
-			}
+		for(String number : numbers) {
+			if(toInt(number) < 0) 
+				 negatives += number;
+			if(toInt(number) < 1001)
+				result += toInt(number);
 		}
 
-		else if(delimiter.equals(",")) {
-			for(String number : numbers) {
-				if(toInt(number) < 0) {
-					 negatives += number;
-				}
-				if(toInt(number) < 1001) {
-					result += toInt(number);
-				}
-			}
-		}
-		if (!negatives.equals("")) {
+		if (!negatives.equals(""))
 			throw new IllegalArgumentException("Negatives not allowed: " + negatives);
-		}
 		return result;
 	}
 }
